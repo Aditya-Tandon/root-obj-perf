142612661b8bbe598f09ce01e2eb8b2908984cb6
Aditya Tandon <adityatandon29@gmail.com>
2026-01-13 17:35:45 +0000

    feat: add apply_custom_cuts func to data_loading_helpers

--- data_loading_helpers.py
+++ data_loading_helpers.py
@@ -136,3 +136,73 @@ def select_gen_b_quarks_from_higgs(events):
 
     print(f"Found {ak.sum(ak.num(gen_b_quarks_from_H))} b-quarks from Higgs decays.")
     return gen_b_quarks_from_H
+
+
+def apply_custom_cuts(reco_jets, config, key, kinematic_only=False):
+    """
+    Apply custom cuts to a jet collection.
+
+    Parameters
+    ----------
+    reco_jets : awkward.Array
+        Jet collection (offline or L1).
+    config : dict
+        Global CONFIG dict.
+    key : str
+        Either "offline" or "l1" to select the appropriate config.
+    kinematic_only : bool
+        If True, only apply kinematic cuts, i.e., pt and eta cuts
+        If False, apply custom tagger cuts as well
+    """
+    subcfg = config[key]
+
+    pt_cut = subcfg["pt_cut"]
+    eta_cut = subcfg["eta_cut"]
+
+    print(f"\nApplying custom pT cut of {pt_cut} GeV for {key} jets...")
+    pt_mask = reco_jets.pt > pt_cut
+    eta_mask = abs(reco_jets.eta) < eta_cut
+    final_mask = pt_mask & eta_mask
+
+    if kinematic_only:
+        pass
+    else:
+        print(f"Applying custom cuts for {tagger_name} ({key})...")
+        b_tag_cut = subcfg["b_tag_cut"]
+        tagger_name = subcfg["tagger_name"]
+
+        if key == "offline":
+            charm_veto_cut = subcfg["charm_veto_cut"]
+            electron_veto_cut = subcfg["electron_veto_cut"]
+            muon_veto_cut = subcfg["muon_veto_cut"]
+
+            if tagger_name.startswith("btagPNet"):
+                b_jet_mask = (reco_jets.btagPNetB > b_tag_cut)
+                charm_veto_mask = (reco_jets.btagPNetCvB < charm_veto_cut)
+                final_mask = final_mask & charm_veto_mask & b_jet_mask
+
+            elif tagger_name.startswith("btagUParTAK4"):
+                b_jet_mask = (reco_jets.btagUParTAK4probb > b_tag_cut)
+                charm_veto_mask = (reco_jets.btagUParTAK4CvB < charm_veto_cut)
+                electron_veto_mask = (reco_jets.btagUParTAK4Ele < electron_veto_cut)
+                muon_veto_mask = (reco_jets.btagUParTAK4Mu < muon_veto_cut)
+                final_mask = (
+                    final_mask
+                    & charm_veto_mask
+                    & electron_veto_mask
+                    & muon_veto_mask
+                    & b_jet_mask
+                )
+
+        elif key == "l1ng":
+            # For L1, just apply the tagger cut generically
+            tag_mask = getattr(reco_jets, tagger_name) > b_tag_cut
+            final_mask = final_mask & tag_mask
+
+        elif key == "l1ext":
+            tag_mask = getattr(reco_jets, tagger_name) > b_tag_cut
+            final_mask = final_mask & tag_mask
+
+    reco_jets = reco_jets[final_mask]
+    return reco_jets
+
